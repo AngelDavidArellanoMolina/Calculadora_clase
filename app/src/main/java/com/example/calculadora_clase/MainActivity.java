7a1d0b113c8d038ee36db0d7a9e4e07a06892318
ÁngelDavidArellanoM <angeldamc11@gmail.com>
2023-10-18 14:15:42 +0200

    spinner

--- app/src/main/java/com/example/calculadora_clase/MainActivity.java
+++ app/src/main/java/com/example/calculadora_clase/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.calculadora_clase;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] tipoConversion = {"Volumen", "Longitud", "Masa", "Velocidad", "Temperatura", "Presión",
+                "Tamaño de Datos"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, tipoConversion);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        Spinner spinner = findViewById(R.id.spinner);
+        spinner.setAdapter(adapter);
+
+        spinner.setSelection(0);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (position == 0) {
+                    // Esto se ejecutará cuando se seleccione la opción por defecto
+                    // Puedes realizar acciones específicas aquí
+                } else {
+                    String item = (String) parent.getItemAtPosition(position);
+                    Toast.makeText(MainActivity.this, "Seleccionaste: " + item, Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // Código para cuando no se selecciona nada
+            }
+        });
     }
 }
